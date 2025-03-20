144a9e1a221838a359098f046b925ad1888e87f8
elsa <elsa@gmail.com>
2025-03-20 12:39:04 +0100

    ServidorFront

--- front/ColeccionPersonal/src/coleccion-rest-service.spec.ts
+++
@@ -1,7 +0,0 @@
-import { ColeccionRestService } from './coleccion-rest-service';
-
-describe('ColeccionRestService', () => {
-  it('should create an instance', () => {
-    expect(new ColeccionRestService()).toBeTruthy();
-  });
-});
