1ab04b987421f850af48436630fa83a3dc2de197
curso06 <nocorreo@mocorreo.com>
2025-03-21 09:05:16 +0100

    Actualizar persona arreglado

--- back/src/main/java/es/santander/ascender/retoGrupoCIC/controller/PersonaController.java
+++ back/src/main/java/es/santander/ascender/retoGrupoCIC/controller/PersonaController.java
@@ -9,12 +9,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import es.santander.ascender.retoGrupoCIC.model.Persona;
 import es.santander.ascender.retoGrupoCIC.service.PersonaService;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @RestController
 @RequestMapping("/api/personas")
@@ -22,7 +22,7 @@ public class PersonaController {
 
     @Autowired
     private PersonaService personaService;
-    
+
     @PostMapping
     public Persona createPersona(Persona persona) {
         return personaService.createPersona(persona);
@@ -40,16 +40,12 @@ public class PersonaController {
 
     @PutMapping("/{id}")
     public ResponseEntity<Persona> updatePersona(@PathVariable Long id, @RequestBody Persona persona) {
-        System.out.println("Solicitud de actualización recibida para ID: " + id);
-        System.out.println("Datos recibidos: " + persona);
-       
-            Persona personaActualizada = personaService.updatePersona(id, persona);
-            return ResponseEntity.ok(personaActualizada);
-     
-           
-        
+
+        Persona personaActualizada = personaService.updatePersona(id, persona);
+        return ResponseEntity.ok(personaActualizada);
+
     }
-    
+
     @DeleteMapping("/{id}")
     public void deletePersona(Long id) {
         personaService.deletePersona(id);
