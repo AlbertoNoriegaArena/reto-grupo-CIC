06b0c362107ca72d5b02ad9c4beba02333615fd1
Iryna Bohomiakova <irinkavakula@gmail.com>
2025-03-24 14:12:13 +0100

    Add change controller musica y libro

--- back/src/main/java/es/santander/ascender/retoGrupoCIC/controller/LibroController.java
+++ back/src/main/java/es/santander/ascender/retoGrupoCIC/controller/LibroController.java
@@ -1,6 +1,7 @@
 package es.santander.ascender.retoGrupoCIC.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -14,11 +15,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import es.santander.ascender.retoGrupoCIC.config.TipoItemNotFoundException;
+import es.santander.ascender.retoGrupoCIC.model.Formato;
 import es.santander.ascender.retoGrupoCIC.model.Libro;
-import es.santander.ascender.retoGrupoCIC.model.TipoItemFormato;
-import es.santander.ascender.retoGrupoCIC.model.TipoItemFormatoId;
+import es.santander.ascender.retoGrupoCIC.model.TipoItem;
+import es.santander.ascender.retoGrupoCIC.service.FormatoService;
 import es.santander.ascender.retoGrupoCIC.service.LibroService;
-import es.santander.ascender.retoGrupoCIC.service.TipoItemFormatoService;
+import es.santander.ascender.retoGrupoCIC.service.TipoItemService;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/libros")
@@ -28,15 +32,52 @@ public class LibroController {
     private LibroService libroService;
 
     @Autowired
-    private TipoItemFormatoService tipoItemFormatoService;
+    private FormatoService formatoService;
 
-    @PostMapping
-    public ResponseEntity<?> create(@RequestBody Libro libro) {
-        TipoItemFormatoId tipoItemFormatoId = new TipoItemFormatoId(libro.getItem().getFormato().getId(), libro.getItem().getTipo().getId());
-        TipoItemFormato tipoItemFormat = tipoItemFormatoService.obtenerTipoItemFormatoPorId(tipoItemFormatoId).orElse(null);
-        if (tipoItemFormat == null) {
-            return new ResponseEntity<>("Tipo y formato ilegal.", HttpStatus.NOT_FOUND);
+    @Autowired
+    private TipoItemService tipoItemService;
+
+   @PostMapping
+    public ResponseEntity<?> create( @Valid @RequestBody Libro libro) {
+        // Buscar el tipo "Libro"
+        Optional<TipoItem> tipoLibroOpt = tipoItemService.obtenerPorNombre("Libro");
+
+        if (tipoLibroOpt.isEmpty()) {
+            return new ResponseEntity<>("Error: Tipo Libro no encontrado", HttpStatus.INTERNAL_SERVER_ERROR);
         }
+
+        TipoItem tipoLibro = tipoLibroOpt.get();
+
+        // Obtener el tipo del item asociado a la libro
+        TipoItem tipoItemRecibido = libro.getItem().getTipo();
+
+        // Verificar si el tipoItem es null
+        if (tipoItemRecibido == null || tipoItemRecibido.getId() == null) {
+            return new ResponseEntity<>("Error: No se ha proporcionado un tipo de ítem válido", HttpStatus.BAD_REQUEST);
+        }
+
+        // Coger el tipoItem en la DDBB
+        Optional<TipoItem> tipoItemBD = tipoItemService.obtenerTipoItemPorId(tipoItemRecibido.getId());
+
+        if (tipoItemBD.isEmpty()) {
+            throw new TipoItemNotFoundException(tipoItemRecibido.getId());
+        }
+
+        // Validar el tipo
+        if (!tipoItemBD.get().getId().equals(tipoLibro.getId())) {
+            return new ResponseEntity<>(
+                    "Error: Solo se pueden crear libros. Has seleccionado: " + tipoItemBD.get().getNombre(),
+                    HttpStatus.BAD_REQUEST);
+        }
+
+        // Validar formato
+        List<Formato> formatosValidos = formatoService.obtenerFormatosPorTipoItem(tipoLibro);
+
+        if (!formatosValidos.contains(libro.getItem().getFormato())) {
+            return new ResponseEntity<>("Formato inválido para libros. Formatos válidos: "
+                    + formatosValidos.stream().map(Formato::getNombre).toList(), HttpStatus.BAD_REQUEST);
+        }
+
         return new ResponseEntity<>(libroService.createLibro(libro), HttpStatus.CREATED);
     }
 
@@ -45,20 +86,29 @@ public class LibroController {
         return libroService.retriveLibro(id);
     }
 
-    @PutMapping("/{id}")
-    public Libro update(@PathVariable("id") Long id, @RequestBody Libro libro) {
-        return libroService.updateLibro(id, libro);
-    }
-
-    @DeleteMapping("/{id}")
-    public String delete(@PathVariable("id") Long id) {
-        libroService.deleteLibro(id);
-        return "delete";
-    }
-
     @GetMapping
     public List<Libro> libro() {
         return libroService.retriveAllLibros();
     }
+
+    // Método para actualizar la musica
+    @PutMapping("/{itemId}")
+    public ResponseEntity<Libro> update(@PathVariable("itemId") Long itemId, @Valid @RequestBody Libro libro) {
+    
+        Libro updatedLibro = libroService.updateLibro(itemId, libro);
+
+        return new ResponseEntity<>(updatedLibro, HttpStatus.OK);
+    }
+
+     @DeleteMapping("/{itemId}")
+     public ResponseEntity<String> delete(@PathVariable("itemId") Long itemId) {
+        try {
+             libroService.deleteLibro(itemId);
+             return new ResponseEntity<>("Libro eliminada", HttpStatus.OK);
+        } catch (IllegalArgumentException e) {
+ 
+             return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+        }     
 
 }
--- back/src/main/java/es/santander/ascender/retoGrupoCIC/controller/MusicaController.java
+++ back/src/main/java/es/santander/ascender/retoGrupoCIC/controller/MusicaController.java
@@ -1,6 +1,7 @@
 package es.santander.ascender.retoGrupoCIC.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -14,11 +15,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import es.santander.ascender.retoGrupoCIC.config.TipoItemNotFoundException;
+import es.santander.ascender.retoGrupoCIC.model.Formato;
 import es.santander.ascender.retoGrupoCIC.model.Musica;
-import es.santander.ascender.retoGrupoCIC.model.TipoItemFormato;
-import es.santander.ascender.retoGrupoCIC.model.TipoItemFormatoId;
+import es.santander.ascender.retoGrupoCIC.model.TipoItem;
+import es.santander.ascender.retoGrupoCIC.service.FormatoService;
 import es.santander.ascender.retoGrupoCIC.service.MusicaService;
-import es.santander.ascender.retoGrupoCIC.service.TipoItemFormatoService;
+import es.santander.ascender.retoGrupoCIC.service.TipoItemService;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/musica")
@@ -28,33 +32,57 @@ public class MusicaController {
     private MusicaService musicaService;
 
     @Autowired
-    private TipoItemFormatoService tipoItemFormatoService;
+    private FormatoService formatoService;
+
+    @Autowired
+    private TipoItemService tipoItemService;
 
     @PostMapping
-    public ResponseEntity<?> create(@RequestBody Musica musica) {
-        TipoItemFormatoId tipoItemFormatoId = new TipoItemFormatoId(musica.getItem().getFormato().getId(), musica.getItem().getTipo().getId());
-        TipoItemFormato tipoItemFormat = tipoItemFormatoService.obtenerTipoItemFormatoPorId(tipoItemFormatoId).orElse(null);
-        if (tipoItemFormat == null) {
-            return new ResponseEntity<>("Tipo y formato ilegal.", HttpStatus.NOT_FOUND);
+    public ResponseEntity<?> create(@Valid @RequestBody Musica musica) {
+        // Buscar el tipo "Musica"
+        Optional<TipoItem> tipoMusicaOpt = tipoItemService.obtenerPorNombre("Musica");
+
+        if (tipoMusicaOpt.isEmpty()) {
+            return new ResponseEntity<>("Error: Tipo Musica no encontrado", HttpStatus.INTERNAL_SERVER_ERROR);    
         }
+
+        TipoItem tipoMusica = tipoMusicaOpt.get();
+
+        // Obtener el tipo del item asociado a la musica
+        TipoItem tipoItemRecibido = musica.getItem().getTipo();
+
+        // Verificar si el tipoItem es null
+        if (tipoItemRecibido == null || tipoItemRecibido.getId() == null) {
+            return new ResponseEntity<>("Error: No se ha proporcionado un tipo de ítem válido", HttpStatus.BAD_REQUEST);
+        }
+
+        // Coger el tipoItem en la DDBB
+        Optional<TipoItem> tipoItemBD = tipoItemService.obtenerTipoItemPorId(tipoItemRecibido.getId());
+
+        if(tipoItemBD.isEmpty()) {
+            throw new TipoItemNotFoundException(tipoItemRecibido.getId());
+        }
+
+        // Validar el tipo
+        if (tipoItemBD.get().getId().equals(tipoMusica.getId())) {
+            return new ResponseEntity<>(
+                "Error: Solo se pueden crear musica. Has seleccionado: " + tipoItemBD.get().getNombre(),
+                 HttpStatus.BAD_REQUEST);
+        }
+
+        // Validar formato
+        List<Formato> formatosValidos = formatoService.obtenerFormatosPorTipoItem(tipoMusica);
+
+        if (!formatosValidos.contains(musica.getItem().getFormato())) {
+            return new ResponseEntity<>("Formato inválido para musica. Formatos válidos:"
+                + formatosValidos.stream().map(Formato::getNombre).toList(), HttpStatus.BAD_REQUEST);   
+        }
+       
         return new ResponseEntity<>(musicaService.createMusica(musica), HttpStatus.CREATED);
-    }
-
-
+    }    
     @GetMapping("/{id}")
     public Musica retrive(@PathVariable("id") Long id) {
         return musicaService.retriveMusica(id);
-    }
-
-    @PutMapping("/{id}")
-    public Musica update(@PathVariable("id") Long id, @RequestBody Musica musica) {
-        return musicaService.updateMusica(id, musica);
-    }
-
-    @DeleteMapping("/{id}")
-    public String delete(@PathVariable("id") Long id) {
-        musicaService.deleteMusica(id);
-        return "delete";
     }
 
     @GetMapping
@@ -62,5 +90,22 @@ public class MusicaController {
         return musicaService.retriveAllMusica();
     }
 
+    @PutMapping("/{itemId}")
+    public ResponseEntity<Musica> update(@PathVariable("itemId") Long itemId, @Valid @RequestBody Musica musica) {
+        
+        Musica updateMusica = musicaService.updateMusica(itemId, musica);
+        
+        return new ResponseEntity<>(updateMusica, HttpStatus.OK);
+    }
 
+    @DeleteMapping("/{itemId}")
+    public ResponseEntity<String> delete(@PathVariable("itemId") Long itemId) {
+        try {
+            musicaService.deleteMusica(itemId);
+            return new ResponseEntity<>("Musica eliminada", HttpStatus.OK);
+        } catch (IllegalArgumentException e) {
+
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
 }
